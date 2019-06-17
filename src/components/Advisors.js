9c9126cd7b3a19ffeb938bff7454de67972f4746
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-16 23:44:29 -0400

    Able to display new advisors form, and all advisors from selected store

--- src/components/Advisors.js
+++ src/components/Advisors.js
@@ -4,12 +4,42 @@ class Advisors extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      name: ''
     }
+  }
+  handleName = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.createAdvisor(this.state)
+    this.clearForm()
   }
   render() {
     return (
-      <h2> yo </h2>
+      <div className="advisors-container">
+        <div className="advisors-form">
+          <h2>ADD A NEW BEAUTY ADVISOR</h2>
+          <form className="form" onSubmit={this.handleSubmit}>
+            <input type="text" name="name" value={this.state.name} placeholder="ADVISOR NAME" autoComplete="off" onChange={this.handleName}/>
+
+            <input type="submit" value="ADD"/>
+          </form>
+        </div>
+        <div className="advisors-list">
+          <h2>ALL ADVISORS</h2>
+          {this.props.advisors.map((advisor, index) => {
+            return (
+              <div className="advisor" key={index}>
+                <button className="delete">X</button>
+                <h3>{advisor.name}</h3>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     )
   }
 }
