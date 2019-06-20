6a4c6e749a334ccdcd036736b716091be6c12e7d
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-06-20 03:24:15 -0400

    Able to click on logo to return to main page from any view

--- src/App.js
+++ src/App.js
@@ -148,7 +148,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1><span className="red">MY</span> <span className="thin">BEAUTY ADVISORS</span></h1>
+        <h1 onClick={()=>{this.setView('stores')}}><span className="red">MY</span> <span className="thin">BEAUTY ADVISORS</span></h1>
         {(()=>{
           if(this.state.view === 'stores') {
             return (
